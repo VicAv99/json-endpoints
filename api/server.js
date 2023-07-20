22e8528015b72db88d016da63015c2daab1a69e7
Victor Avila <vicav99@gmail.com>
2023-07-19 21:24:29 -0700

    feat: misc

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // See https://github.com/typicode/json-server#module
 const fs = require("fs");
 const path = require("path");
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const db = JSON.parse(fs.readFileSync(path.join("db.json")));
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(db);
